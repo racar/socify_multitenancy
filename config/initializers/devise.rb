0de8258f194f5c3666be940e02cd54a141886968
Rafael Carrascal R <rcarrascal@gmail.com>
2017-04-21 06:40:14 -0500

    changing email sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'administrador@opensky.com.co'
+  config.mailer_sender = 'pa@opensky.com.co'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
