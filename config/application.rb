30e6ab4187fb84a1366f93a1f98ec44a3719b70a
Rafael Carrascal R <rcarrascal@gmail.com>
2017-04-24 01:50:52 -0500

    try other length in domain

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Socify
     config.middleware.use Rack::MethodOverride
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-    config.action_dispatch.tld_length = 2
+    config.action_dispatch.tld_length = 3
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
