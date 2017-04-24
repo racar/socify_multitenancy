b07cf6aa664f1f380ad72a9df3aa08b19ee5ce54
Rafael Carrascal R <rcarrascal@gmail.com>
2017-04-24 12:36:08 -0500

    No necessary, DNS with other domain

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -90,4 +90,4 @@ end
 # Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
 Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
 # Rails.application.config.middleware.use 'Apartment::Elevators::FirstSubdomain'
-Apartment::Elevators::Subdomain.excluded_subdomains = ['sennovakuberastaging']
+#Apartment::Elevators::Subdomain.excluded_subdomains = ['sennovakuberastaging']
