9cb52603890133914065c50c9f5a6bd3848ae95f
Rafael Carrascal R <rcarrascal@gmail.com>
2017-04-27 21:57:19 -0500

    enable user params

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,6 +3,10 @@ ActiveAdmin.register User do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
+permit_params :name,:email,:encrypted_password,:about,:avatar,:cover,:reset_password_token,:reset_password_sent_at,
+:remember_created_at,:sign_in_count,:current_sign_in_at,:last_sign_in_at,:current_sign_in_ip,
+  :last_sign_in_ip,:confirmation_token,:confirmed_at,:confirmation_sent_at,:created_at,
+  :updated_at,:sex,:location,:dob,:phone_number,:posts_count,:slug,:points,:subdomain
 #
 # or
 #
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,9 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Socify"
+  config.site_title = "SennovaKubera"
+
+  
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
