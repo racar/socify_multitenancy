a8fc3424ca163dc678a579e5f83d7d65186338ab
Rafael Carrascal R <rcarrascal@gmail.com>
2017-05-22 10:18:01 -0500

    FIX aws error, folder

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
       region:                'us-east-1', # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = 'socify' # required
+    config.fog_directory  = 'sennovakubera' # required
     config.fog_public     = true # optional, defaults to true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   end
