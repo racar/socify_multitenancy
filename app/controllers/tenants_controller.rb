b73540d6724a175b2c300ec810072cc3596a4a7c
Rafael Carrascal R <rcarrascal@gmail.com>
2017-06-12 15:29:24 -0500

    Fix some reported errors

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,10 +1,27 @@
 class TenantsController < InheritedResources::Base
  layout 'landing'
+
+ def create
+   params[:domain]
+   #begin
+    # byebug
+   Tenant.create
+    #byebug
+   AdminUser.create!(email: 'racar@example.com', password: 'password', password_confirmation: 'password')
+   #rescue => e
+     #byebug
+    # flash[:notice] = "El dominio ingresado ya existe"
+    # redirect_to :controller => 'tenants', :action => 'new'
+   #end
+
+ end
+
+
   private
 
     def tenant_params
       params.require(:tenant).permit(:domain, :owner)
     end
 
-  
+
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,6 +2,8 @@ class Tenant < ApplicationRecord
   after_create :create_tenant
   def create_tenant
     #Apartment::Tenant.create(subdomain)
+    byebug
     Apartment::Tenant.create(domain)
+
   end
 end
