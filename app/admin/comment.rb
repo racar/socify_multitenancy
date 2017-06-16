bbb435c56a4b4d1d9936ace128568e6143e272f0
Rafael Carrascal R <rcarrascal@gmail.com>
2017-06-16 09:05:07 -0500

    ActiveAdmin Error in edit

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register Comment, as: "UserComment" do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
+permit_params :title,:commentable_type,:commentable_id,:user_id,:role,:created_at,:updated_at,:comment_html
 #
 # or
 #
